fd068f38cd50c390a48cfbd20fc8384a93d5fd4c
mortenjust <morten.just@genstart.dk>
2016-03-29 01:00:58 -0700

    add trump mode

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let str:NSString = "Let's get to the bottom of these strings. Two is where the fish go. Three is where they hang out. Four is where they play. Five is five. But how long is a line really?"
+
+func wordRangeAtIndex(index:Int, inString str:NSString) -> NSRange {
+    let tagger = NSLinguisticTagger(tagSchemes: [NSLinguisticTagSchemeTokenType], options: 0)
+    var r : NSRange = NSMakeRange(0,0)
+    tagger.string = str as String
+    tagger.tagAtIndex(index, scheme: NSLinguisticTagSchemeTokenType, tokenRange: &r, sentenceRange: nil )
+    return r
+}
+
+
+func wordAtIndex(index:Int, inString str:NSString) -> NSString {
+    return str.substringWithRange(wordRangeAtIndex(index, inString: str))
+}
+
+wordAtIndex(20, inString: str)
+
+
