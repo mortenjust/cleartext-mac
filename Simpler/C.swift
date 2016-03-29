bc1057e33cfb7d38800bb6dd3439a0c59c29da60
Florian Heiber <flo@flohei.de>
2016-03-29 13:18:07 +0200

    Adding German Word List
    Using the list from Uni Leipzig from 2001 since this was the only one I could find. http://wortschatz.uni-leipzig.de/Papers/top1000de.txt

--- Simpler/C.swift
+++ Simpler/C.swift
@@ -39,7 +39,8 @@ class C: NSObject {
         Language(code: "en", name: "en"),
         Language(code: "xkcd", name: "xkcd"),
         Language(code: "jobs", name: "jobs"),
-        Language(code: "hemingway", name: "hemingway")
+        Language(code: "hemingway", name: "hemingway"),
+        Language(code: "de", name: "de")
     ]
     
     
