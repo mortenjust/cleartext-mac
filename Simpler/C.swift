dd2edf86a287de45ee700bca413b9081d7f9caaa
Eric Jensen <jensen39@gmail.com>
2016-06-08 09:51:04 -0700

    Rename Jobs and Hemingway to match their dictionary filename. The previous mismatch caused the dictionaries to fail to load.

--- Simpler/C.swift
+++ Simpler/C.swift
@@ -56,8 +56,8 @@ class C: NSObject {
         Language(code: "русский", name: "русский"),
         Language(code: "Trump", name: "Trump"),
         Language(code: "xkcd", name: "xkcd"),
-        Language(code: "jobs", name: "jobs"),
-        Language(code: "hemingway", name: "hemingway"),        
+        Language(code: "Jobs", name: "Jobs"),
+        Language(code: "Hemingway", name: "Hemingway"),
     ]
     
     
